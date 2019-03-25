df2845e31a6e40484aaf58a8b0907c9906dd3c09
JoelynZhou <zhoujialin@ezbuy.com>
2019-03-25 20:17:48 +0800

    合并排序数组问题

--- 88.js
+++ 88.js
@@ -6,22 +6,20 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-  let [i, j] = [0, 0];
-  while (i < nums1.length && j < nums2.length) {
-    if (nums2[j] > nums1[i]) {
-      console.log("i,j,nums1[i],nums2[j]", i, j, nums1[i], nums2[j]);
-      nums1 = nums1.slice(i, 0, nums2[j]);
-      console.log("nums1", nums1);
-      j++;
-    } else if (nums2[j] >= nums1[i]) {
-      i++;
+  let [i, j, k] = [m - 1, n - 1, m + n - 1];
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
     }
   }
-  console.log(nums1);
-  //   return dest.concat(nums1.slice(i), nums2.slice(j));
+  while (j >= 0) {
+    nums1[k--] = nums2[j--];
+  }
 };
 
-const nums1 = [1, 2, 3, 0, 0, 0];
-const nums2 = [2, 5, 6];
-const [m, n] = [3, 3];
+const nums1 = [0];
+const nums2 = [1];
+const [m, n] = [0, 1];
 console.log(merge(nums1, m, nums2, n));
