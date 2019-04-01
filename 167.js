a33da64fd44eecd05dea051146468eda275cf141
JoelynZhou <zhoujialin@ezbuy.com>
2019-04-01 21:28:58 +0800

    两数之和问题

---
+++ 167.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  let i = 0;
+  let j = numbers.length - 1;
+  while (i < j) {
+    let current = numbers[i] + numbers[j];
+    if (current === target) {
+      return [i + 1, j + 1];
+    } else if (current > target) {
+      j--;
+    } else {
+      i++;
+    }
+  }
+};
+
+const input = [2, 7, 11, 15];
+console.log(twoSum(input, 9));
