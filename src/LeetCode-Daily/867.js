34799e906dd753c6e8c0930812347345490ecc61
JoelynZhou <joelynzhou@hotmail.com>
2021-02-25 11:27:49 +0800

    暴力穷举

--- src/LeetCode-Daily/867.js
+++ src/LeetCode-Daily/867.js
@@ -2,21 +2,21 @@
  * @param {number[][]} A
  * @return {number[][]}
  */
-var transpose = function (A) {
-    let list = [];
-    for (let i = 0; i < A[0].length; i++) {
-        let row = [];
-        for (let j = 0; j < A.length; j++) {
-            row.push(A[j][i]);
-        }
-        list.push(row);
-    }
-    return list;
+var transpose = function(A) {
+	let list = [];
+	for (let i = 0; i < A[0].length; i++) {
+		let row = [];
+		for (let j = 0; j < A.length; j++) {
+			row.push(A[j][i]);
+		}
+		list.push(row);
+	}
+	return list;
 };
 
-const A = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-];
+var transpose1 = function(A) {
+	return Array.from({ length: A[0].length }, (_, i) => A.map((v) => v[i]));
+};
+
+const A = [ [ 1, 2, 3 ], [ 4, 5, 6 ], [ 7, 8, 9 ] ];
 console.log(transpose(A));
--- src/LeetCode-Daily/978.js
+++ src/LeetCode-Daily/978.js
@@ -5,7 +5,7 @@
  * 时间复杂度：O(N)
  * 空间复杂度：O(1)
  */
-var maxTurbulenceSize = function(arr) {
+var maxTurbulenceSize1 = function(arr) {
 	let ret = 1; // 记录当前最大的湍流子数组长度
 	let left = 0,
 		right = 0;
@@ -33,5 +33,27 @@ var maxTurbulenceSize = function(arr) {
 	}
 	return ret;
 };
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ * 方案 2：动态规划
+ * 时间复杂度：O(N)
+ */
+var maxTurbulenceSize2 = function(arr) {
+	let ret = 1;
+	let up = Array(arr.length).fill(1),
+		down = Array(arr.length).fill(1);
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] > arr[i - 1]) {
+			up[i] = down[i - 1] + 1;
+		} else if (arr[i] < arr[i - 1]) {
+			down[i] = up[i - 1] + 1;
+		}
+		ret = Math.max(ret, Math.max(up[i], down[i]));
+	}
+	return ret;
+};
+
 console.log(maxTurbulenceSize([ 9, 4, 2, 10, 7, 8, 8, 1, 9 ]));
 // console.log(maxTurbulenceSize([ 0, 8, 45, 88, 48, 68, 28, 55, 17, 24 ]));
