13d0677b713c2ee3fe89a0500455d2ff5f680b11
JoelynZhou <joelynzhou@hotmail.com>
2021-07-20 19:16:54 +0800

    :sparkles: 70. 爬楼梯

---
+++ src/LeetCode-Daily/70.js
@@ -0,0 +1,16 @@
+/**
+ * 70. 爬楼梯
+ * 经典 dp 问题
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+	let dp = Array(n + 1);
+	dp[0] = 1;
+	dp[1] = 1;
+	dp[2] = 2;
+	for (let i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	return dp[n];
+};
