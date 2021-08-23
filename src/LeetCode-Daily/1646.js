521f7e0507aedc9c94d8c6484ff1d08ea8206605
JoelynZhou <joelynzhou@hotmail.com>
2021-08-23 16:33:38 +0800

    feat: 1646. 获取生成数组中的最大值

---
+++ src/LeetCode-Daily/1646.js
@@ -0,0 +1,23 @@
+/**
+ * 1646. 获取生成数组中的最大值
+ * 思路：最大值一定是最后两个数+ 动规
+ * @param {number} n
+ * @return {number}
+ */
+var getMaximumGenerated = function (n) {
+    let arr = Array.from(n + 1);
+    arr[0] = 0; arr[1] = 1;
+    let max = 0;
+    if (n < 2) {
+        return n;
+    }
+    for (let i = 2; i <= n; i++) {
+        if (i % 2 === 0) {
+            arr[i] = arr[i / 2];
+        } else {
+            arr[i] = arr[Math.floor(i / 2)] + arr[Math.ceil(i / 2)];
+        }
+        max = Math.max(max, arr[i]);
+    }
+    return max;
+};
