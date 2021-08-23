e8ee382cb754dae47950a3bfcb5a6feca019b058
JoelynZhou <joelynzhou@hotmail.com>
2021-08-23 17:01:41 +0800

    feat: 神奇的位运算

--- src/LeetCode-Daily/231.js
+++ src/LeetCode-Daily/231.js
@@ -1,6 +1,6 @@
 /**
  * 231. 2 的幂
- * 边界判断
+ * 思路 1. 常规思路 + 边界判断
  * @param {number} n
  * @return {boolean}
  */
@@ -18,4 +18,20 @@ var isPowerOfTwo = function (n) {
     }
     return true;
 };
-console.log(isPowerOfTwo(12))
+
+/**
+ * 231. 2 的幂
+ * 思路 1. 位运算
+ * 若 n 是 2 的幂，则 n 二进制一定表示为 1000…00，n-1 一定标识为 0111…11，则 n & n-1 = 0 必然成立
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo2 = function (n) {
+    if (n<=0) {
+        return false;
+    }
+    console.log(n & (n - 1),n & (n - 1)===0)
+    return (n & (n - 1)) === 0;
+};
+
+console.log(isPowerOfTwo2(16))
