871b052e4acdd69c04211857c9a30a82a5682e72
JoelynZhou <joelynzhou@hotmail.com>
2021-11-01 10:51:48 +0800

    feat: 数组去重

---
+++ src/LeetCode-Daily/500/570/575.js
@@ -0,0 +1,13 @@
+/**
+ * 575. 分糖果
+ * 思路：把所有糖果的种类都给妹妹一遍，
+ * 如果种类不够多，则妹妹能拿到所有种类的数量；
+ * 如果种类足够多，则妹妹能拿多少种类算多少种类；
+ * @param {number[]} candyType
+ * @return {number}
+ */
+var distributeCandies = function (candyType) {
+	const diffType = Array.from(new Set(candyType));
+	return Math.min(candyType.length / 2, diffType.length);
+};
+console.log(distributeCandies([1, 1, 2, 2, 3, 3]));
