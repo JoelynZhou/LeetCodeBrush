0fd00b4d5b044783d03934205bc9bf3b2b4bb590
JoelynZhou <joelynzhou@hotmail.com>
2021-11-02 11:39:23 +0800

    feat: 思路-和下一个节点交换

---
+++ src/LeetCode-Daily/200/230/237.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * 237. 删除链表中的节点
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+	node.val = node.next.val; // 将当前节点的值修改成下一个节点的值
+	node.next = node.val.next; // 删掉下一个节点
+};
